61167f925342f4e063aba4bc023d25f898f163c9
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-06-12 12:39:35 -0300

    Update jfltesetup.sh

--- jfltesetup.sh
+++ jfltesetup.sh
@@ -10,19 +10,8 @@ cd build
 pstest 7717/2
 cd $root
 
-cd device/samsung/jf-common
-pstest 8855/1
-cd $root
-
-cd packages/apps/Camera
-pstest 8875/1
-cd $root
-
-cd vendor/aokp 
-pstest 8191/8
-pstest 8854/2
-pstest 8658/4
-pstest 8890/1
+cd device/samsung/msm8960-common
+pstest 8924/2
 cd $root
 
 . build/envsetup.sh >/dev/null 2>&1
