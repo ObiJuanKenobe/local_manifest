1450e871c22cbf4950becadc741de46362c1d82b
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-07-03 23:19:43 -0400

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -30,24 +30,27 @@ cd $root
 cd frameworks/base
 pstest 5133/3
 pstest 9282/1
+pstest 9301/1
+pstest 8562/3
+pstest 7600/3
 cd $root
 
 cd hardware/libhardware
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_libhardware refs/changes/83/44783/4 && git cherry-pick FETCH_HEAD
 cd $root
 
+cd packages/apps/Mms
+psest 9291/1
+cd $root
+
 cd packages/apps/ROMControl
 pstest 9142/16
-pstest 9132/5
+# soft key RC side has FC
+pstest 7601/3
 cd $root
 
 cd vendor/aokp
 pstest 8943/1
-cd $root
-
-cd vendor/samsung
-pstest 9220/2
-pstest 9223/1
 cd $root
 
 # Reinitialize build environment with pspush
